6ea54e63ab2f858341cd55967a6e276a6b09c7bb
msyon2 <suyon.korea@gmail.com>
2022-04-11 17:31:39 +0900

    first commit

---
+++ js/jq.js
@@ -0,0 +1,92 @@
+const topMenu = $("nav ul.gnb>li");
+//button click시 이동
+topMenu.click(function (e) {
+  e.preventDefault();
+  let target = $(this);
+  let index = target.index();
+  let section = $(".section").eq(index);
+  let offset = section.offset().top;
+  //offset gets coordinate value
+  $("html,body").animate({ scrollTop: offset }, 1000, "easeOutCirc");
+});
+
+$(window).scroll(function () {
+  let scrollTop = $(window).scrollTop();
+  if (scrollTop >= $("#section1").offset().top - 500) {
+    $("nav ul.gnb li")
+      .eq(0)
+      .addClass("active")
+      .siblings()
+      .removeClass("active");
+  }
+  if (scrollTop >= $("#section2").offset().top - 500) {
+    $("nav ul.gnb li")
+      .eq(1)
+      .addClass("active")
+      .siblings()
+      .removeClass("active");
+    $("#section2").find(".left").addClass("in");
+    //$("#section2").find(".left").css("left", "-200%").stop().animate({"left":0},2000);
+  }
+  if (scrollTop >= $("#section3").offset().top - 500) {
+    $("nav ul.gnb li")
+      .eq(2)
+      .addClass("active")
+      .siblings()
+      .removeClass("active");
+  }
+});
+
+/* SCROLL Events */
+function stickyFn() {
+  const nav = $("nav");
+  $(window).scroll(function () {
+    if ($(window).scrollTop() >= 10) {
+      nav.addClass("sticky");
+    } else {
+      nav.removeClass("sticky");
+    }
+  });
+}
+stickyFn();
+
+
+
+/* ABOUT Page Tab */
+let aboutPg = $("#section1"),
+  tabList = aboutPg.find(".tab li"),
+  tabPanel = aboutPg.find(".tab_content>div");
+
+tabList.each(function (i, e) {
+  //console.log(i,e); //(index no, 값)
+  let tab = $(this); //this = 순환하고 있는 요소중 현재 번째
+  let link = tab.find("a").attr("href");
+  let panel = link.substr(1);
+  tab.click(function (e) {
+    e.preventDefault();
+    tabList.removeClass("active");
+    tab.addClass("active");
+    tabPanel.hide();
+    $("#"+panel).show(); 
+  });
+});
+tabPanel.eq(0).show();
+tabList.eq(0).addClass("active");
+
+
+
+
+/* project mockup hover action */
+$(".hidden").hover(
+  function () {
+    let ah = $(this).innerHeight();
+    let img = $(this).find("img");
+    let imgh = img.innerHeight();
+    console.log(`a height:${ah} img height:${imgh}`);
+    img.stop().animate({ top: ah - imgh }, 4000);
+  },
+  function () {
+    let img = $(this).find("img");
+    img.stop().animate({ top: 0 }, 4000);
+  }
+);
