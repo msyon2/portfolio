2fd661af5ee39a9d2071a5b90b391040a7261fc4
msyon2 <suyon.korea@gmail.com>
2022-05-02 09:25:01 +0900

    misc. adjustments & media folder

--- js/jq.js
+++ js/jq.js
@@ -1,7 +1,7 @@
 const topMenu = $("nav ul.gnb>li");
-const sections = $(".section")
-const prjSections = $("#section2 .portfolio")
-const speed = 500
+const sections = $(".section");
+const prjSections = $("#section2 .portfolio");
+const speed = 500;
 //button click시 이동
 topMenu.click(function (e) {
   e.preventDefault();
@@ -13,15 +13,12 @@ topMenu.click(function (e) {
   $("html,body").animate({ scrollTop: offset }, 1000, "easeOutCirc");
 });
 
-
 //custom cursor
 $(window).on({
   mousemove: function (e) {
-    gsap.to("#cursor", {duration: .2, left: e.pageX -4, top: e.pageY -4})
-  }
-})
-
-
+    gsap.to("#cursor", { duration: 0.2, left: e.pageX - 4, top: e.pageY - 4 });
+  },
+});
 
 /* ***SCROLL Events*** */
 //sticky header
@@ -37,32 +34,27 @@ function stickyFn() {
 }
 stickyFn();
 
-
 $(window).on("scroll", function () {
   //scroll activate gnb
-  let scrollTop = $(window).scrollTop()
-  sections.each(function (i,o) {
+  let scrollTop = $(window).scrollTop();
+  sections.each(function (i, o) {
     if (scrollTop >= sections.eq(i).offset().top - speed) {
       $("nav ul.gnb li")
-      .eq(i)
-      .addClass("active")
-      .siblings()
-      .removeClass("active");
+        .eq(i)
+        .addClass("active")
+        .siblings()
+        .removeClass("active");
     }
-  })
- 
+  });
 
-  //project section slide-ins 
-  prjSections.each(function (i,o){
+  //project section slide-ins
+  prjSections.each(function (i, o) {
     if (scrollTop >= prjSections.eq(i).offset().top - speed) {
-      prjSections.eq(i).find('.left').addClass('in');
-		  prjSections.eq(i).find('.right').addClass('in');
+      prjSections.eq(i).find(".left").addClass("in");
+      prjSections.eq(i).find(".right").addClass("in");
     }
-  })
+  });
 });
-
-
-
 
 /* ** ABOUT Page ** */
 //tab
@@ -71,34 +63,31 @@ let aboutPg = $("#section1"),
   tabList = aboutPg.find(".tab li"),
   tabPanel = aboutPg.find(".tab_content>div");
 
-  $(tabList).on({
-    click:function (e) {
-      let tab = $(this); //this = 순환하고 있는 요소중 현재 번째
-      let link = tab.find("a").attr("href");
-      let panel = link.substr(1); 
-      e.preventDefault();
+$(tabList).on({
+  click: function (e) {
+    let tab = $(this);
+    let link = tab.find("a").attr("href");
+    let panel = link.substr(1);
+    e.preventDefault();
     tabList.removeClass("active");
     tab.addClass("active");
     tabPanel.hide();
-    $("#"+panel).show(); 
-
+    $("#" + panel).show();
 
     //Active tab triggers progressanimation
-  if(tabList.eq(1).hasClass("active")){
-    progressAnimation();
-  }
+    if (tabList.eq(1).hasClass("active")) {
+      progressAnimation();
     }
-  })
-  tabPanel.eq(0).show();
-  tabList.eq(0).addClass("active");
-
-
+  },
+});
+tabPanel.eq(0).show();
+tabList.eq(0).addClass("active");
 
 //skills progress bar activate on scroll
-let animationOst = $("#skills").offset().top - 600;
+/* let animationOst = $("#skills").offset().top - 600;
 const progressWrap = $(".bar");
 let isAni = false;
-
+ */
 /* $(window).scroll(function () {
   if ($(window).scrollTop() >= animationOst && !isAni) {
     //start the animation after specified time
@@ -117,7 +106,10 @@ function progressAnimation(params) {
       progressRate = progressText.attr("data-rate");
     //console.log(progressRate);
 
-    progressBar.stop().delay(300).animate({ width: progressRate + "%" }, 2500); //2.5s
+    progressBar
+      .stop()
+      .delay(300)
+      .animate({ width: progressRate + "%" }, 2500);
 
     let text = function () {
       $({ rate: 0 }).animate(
@@ -127,17 +119,17 @@ function progressAnimation(params) {
           progress: function () {
             let now = this.rate;
             //console.log(now);
-			progressText.text(Math.ceil(now) + "%");
+            progressText.text(Math.ceil(now) + "%");
           },
-		  complete: function(){isAni=true}
-        })
-    }
-    text()
-  })
+          complete: function () {
+            isAni = true;
+          },
+        }
+      );
+    };
+    text();
+  });
 }
-
-
-
 
 /* project mockup hover action */
 $(".hidden").hover(
@@ -154,15 +146,11 @@ $(".hidden").hover(
   }
 );
 
-
-
 /* gallery open toggle*/
-const cards = $('.work');
-  cards.on({
-    click: function (e) {
-      e.preventDefault();
-      $(this).toggleClass("open");
-      
-    }
-  })
-
+const cards = $(".work");
+cards.on({
+  click: function (e) {
+    e.preventDefault();
+    $(this).toggleClass("open");
+  },
+});
